f3c24a4ab53d0ce3e6b951e91047de8d31b8e8ec
Tony Cho <backing.dev@gmail.com>
2025-02-12 15:08:37 +0900

    Swagger installed

--- src/main/java/com/example/demo/CourseTrackerSpringBootApplication.java
+++ src/main/java/com/example/demo/CourseTrackerSpringBootApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CourseTrackerSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseTrackerSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customizeOpenAPI(@Value("{app.description}") String appDescription,
+									@Value("{app.version}") String appVersion) {
+		return new OpenAPI().info(new Info().title("Course Tracker API").version(appVersion)
+											.description(appDescription)
+											.termsOfService("http://swagger.io/terms/")
+											.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Course;
 import com.example.demo.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -10,41 +13,56 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/courses/")
 @RequiredArgsConstructor
+@Tag(name = "Course Controller", description = "This REST controller provide services to manage courses in the Course Tracker application")
 public class CourseController {
 
     private final CourseService courseService;
 
     @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides all courses available in the Course Tracker application")
     public Iterable<Course> getAllCourses() {
         return courseService.getCourses();
     }
 
     @GetMapping("{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides course details for the supplied course id from the Course Tracker application")
     public Optional<Course> getCourseById(@PathVariable("id") long id) {
         return courseService.getCourseById(id);
     }
 
     @GetMapping("category/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides course details for the supplied course category from the Course Tracker application")
     public Iterable<Course> getCoursesByCategory(@PathVariable("name") String category) {
         return courseService.getCoursesByCategory(category);
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @Operation(summary = "Creates a new course in the Course Tracker application")
     public Course createCourse(@RequestBody Course course) {
         return courseService.createCourse(course);
     }
 
     @PutMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Updates the course details in the Course Tracker application for the supplied course id")
     public void updateCourse(@PathVariable("id") long id, @RequestBody Course course) {
         courseService.updateCourse(id, course);
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes the course details for the supplied course id from the Course Tracker application")
     void deleteCourseById(@PathVariable("id") long courseId) {
         courseService.deleteCourseById(courseId);
     }
 
     @DeleteMapping
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes all courses from the Course Tracker application")
     void deleteCourses() {
         courseService.deleteCourses();
     }
